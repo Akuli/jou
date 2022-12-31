b338136807464a548ac46c6c76af7752ab5c0d80
Akuli <akuviljanen17@gmail.com>
2022-12-31 17:05:17 +0200

    Remove unused variables.

--- src/simplify_cfg.c
+++ src/simplify_cfg.c
@@ -82,6 +82,59 @@ static void remove_unreachable_blocks(struct CfGraph *cfg, bool *did_something)
     free(todo.ptr);
 }
 
+static void remove_unused_variables(struct CfGraph *cfg, bool *did_something)
+{
+    char *used = calloc(1, cfg->variables.len);
+
+    for (struct CfBlock **b = cfg->all_blocks.ptr; b < End(cfg->all_blocks); b++) {
+        for (struct CfInstruction *ins = (*b)->instructions.ptr; ins < End((*b)->instructions); ins++) {
+            if (ins->destvar)
+                used[find_var_index(cfg, ins->destvar)] = true;
+
+            switch(ins->kind) {
+            case CF_TRUE:
+            case CF_FALSE:
+            case CF_CHAR_CONSTANT:
+            case CF_INT_CONSTANT:
+            case CF_STRING_CONSTANT:
+                break;
+            case CF_ADDRESS_OF_VARIABLE:
+            case CF_BOOL_NEGATE:
+            case CF_CAST_TO_BIGGER_SIGNED_INT:
+            case CF_CAST_TO_BIGGER_UNSIGNED_INT:
+            case CF_LOAD_FROM_POINTER:
+            case CF_VARCPY:
+                used[find_var_index(cfg, ins->data.operands[0])] = true;
+                break;
+            case CF_INT_ADD:
+            case CF_INT_EQ:
+            case CF_INT_LT:
+            case CF_INT_MUL:
+            case CF_INT_SDIV:
+            case CF_INT_SUB:
+            case CF_INT_UDIV:
+            case CF_STORE_TO_POINTER:
+                used[find_var_index(cfg, ins->data.operands[0])] = true;
+                used[find_var_index(cfg, ins->data.operands[1])] = true;
+                break;
+            case CF_CALL:
+                for (int i = 0; i < ins->data.call.nargs; i++)
+                    used[find_var_index(cfg, ins->data.call.args[i])] = true;
+                break;
+            }
+        }
+    }
+
+    for (int i = cfg->variables.len - 1; i>=0; i--) {
+        if (!used[i]) {
+            cfg->variables.ptr[i] = Pop(&cfg->variables);
+            *did_something = true;
+        }
+    }
+
+    free(used);
+}
+
 static void mark_analyzable_variables(struct CfGraph *cfg, bool *did_something)
 {
     char *analyzable = malloc(cfg->variables.len);
@@ -134,9 +187,10 @@ static void clean_branches_where_condition_always_true_or_always_false(struct Cf
 static void simplify_cfg(struct CfGraph *cfg)
 {
     void (*simplifiers[])(struct CfGraph *, bool *) = {
-        remove_unreachable_blocks,
+        remove_unused_variables,
         mark_analyzable_variables,
-        clean_branches_where_condition_always_true_or_always_false
+        clean_branches_where_condition_always_true_or_always_false,
+        remove_unreachable_blocks,
     };
     int n = sizeof(simplifiers) / sizeof(simplifiers[0]);
 
