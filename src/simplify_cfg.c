ec28e6bd7d6d224af354f6914de5538bea6258c0
Akuli <akuviljanen17@gmail.com>
2023-01-05 20:49:51 +0200

    Return value errors.

--- src/simplify_cfg.c
+++ src/simplify_cfg.c
@@ -472,10 +472,55 @@ static void warn_about_undefined_variables(struct CfGraph *cfg)
     free(statuses);
 }
 
-static void simplify_cfg(struct CfGraph *cfg)
+static void error_about_missing_return(struct CfGraph *cfg, const struct Signature *sig)
+{
+    if (!sig->returntype)
+        return;
+
+    enum VarStatus **statuses = determine_var_statuses(cfg);
+
+    // When a function returns a value, it is stored in a variable named "return".
+    int varidx = -1;
+    for (int i = 0; i < cfg->variables.len; i++) {
+        if (!strcmp(cfg->variables.ptr[i]->name, "return")) {
+            varidx = i;
+            break;
+        }
+    }
+    assert(varidx != -1);
+
+    int blockidx = find_block_index(cfg, &cfg->end_block);
+    switch(statuses[blockidx][varidx]) {
+    case VS_UNVISITED:
+        assert(0);
+    case VS_TRUE:
+    case VS_FALSE:
+    case VS_DEFINED:
+    case VS_UNPREDICTABLE:
+        break;
+    case VS_POSSIBLY_UNDEFINED:
+        show_warning(
+            sig->location,
+            "function '%s' doesn't seem to return a value in all cases", sig->funcname);
+        break;
+    case VS_UNDEFINED:
+        fail_with_error(
+            sig->location,
+            "function '%s' must return a value, because it is defined with '-> %s'",
+            sig->funcname, sig->returntype->name);
+        break;
+    }
+
+    for (int i = 0; i < cfg->all_blocks.len; i++)
+        free(statuses[i]);
+    free(statuses);
+}
+
+static void simplify_cfg(struct CfGraph *cfg, const struct Signature *sig)
 {
     clean_jumps_where_condition_always_true_or_always_false(cfg);
     remove_unreachable_blocks(cfg);
+    error_about_missing_return(cfg, sig);
     remove_unused_variables(cfg);
     warn_about_undefined_variables(cfg);
 }
@@ -484,5 +529,5 @@ void simplify_control_flow_graphs(const struct CfGraphFile *cfgfile)
 {
     for (int i = 0; i < cfgfile->nfuncs; i++)
         if (cfgfile->graphs[i])
-            simplify_cfg(cfgfile->graphs[i]);
+            simplify_cfg(cfgfile->graphs[i], &cfgfile->signatures[i]);
 }
