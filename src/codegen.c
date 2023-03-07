49b70901b496b730b77d07e9033d12b95416e6a6
Akuli <akuviljanen17@gmail.com>
2023-03-07 13:46:07 +0200

    tests/should_succeed/string_syntax.jou works

--- src/codegen.c
+++ src/codegen.c
@@ -498,15 +498,27 @@ static LLVMValueRef build_expression(const struct State *st, const AstExpression
         temp = build_expression(st, expr->data.as.obj);
         result = build_cast(st, temp, get_type_after_cast(st, expr->data.as.obj), get_expr_types(st, expr)->type);
         break;
+    case AST_EXPR_DEREF_AND_GET_FIELD:
+    case AST_EXPR_INDEXING:
+        /*
+        To evaluate foo->bar, we first evaluate &foo->bar and then dereference.
+        We can similarly evaluate &foo[bar].
+
+        This technique cannot be used with all expressions. For example, &(1+2)
+        doesn't work, and &foo.bar doesn't work either whenever &foo doesn't work.
+        But &foo->bar and &foo[bar] always work, because foo is already a pointer
+        and we only add a memory offset to it.
+        */
+        temp = build_address_of_expression(st, expr);
+        result = LLVMBuildLoad(st->builder, temp, "dereffed");
+        break;
     case AST_EXPR_ARRAY:
     case AST_EXPR_BRACE_INIT:
     case AST_EXPR_CALL_METHOD:
     case AST_EXPR_DEREFERENCE:
     case AST_EXPR_DEREF_AND_CALL_METHOD:
-    case AST_EXPR_DEREF_AND_GET_FIELD:
     case AST_EXPR_GET_ENUM_MEMBER:
     case AST_EXPR_GET_FIELD:
-    case AST_EXPR_INDEXING:
     case AST_EXPR_SIZEOF:
         printf("%d\n", expr->kind);
         assert(0);
