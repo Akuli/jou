0d2e807de1980a1f9559628b1767b00f4b2eb401
Akuli <akuviljanen17@gmail.com>
2023-03-11 01:27:24 +0200

    compare_compilers.sh improvements

--- compare_compilers.sh
+++ compare_compilers.sh
@@ -4,6 +4,8 @@
 # They should be able to tokenize and parse each Jou file in exactly the same way.
 # If tokenizing/parsing a Jou file fails, both compilers should fail with the same error message.
 
+set -e
+
 if [ "$1" = --fix ]; then
     fix=yes
     shift
@@ -29,10 +31,47 @@ else
     make
 fi
 
-set -e
-
 rm -rf tmp/compare_compilers
 mkdir -vp tmp/compare_compilers
+
+function append_line()
+{
+    local file="$1"
+    local line="$2"
+    echo "  Adding $line to $file"
+
+    if [ grep -q $'\r' $error_list_file ]; then
+        # CRLF line endings (Windows, but also depends on git settings)
+        printf "%s\r\n" "$line" >> "$file"
+    else
+        printf "%s\n" "$line" >> "$file"
+    fi
+}
+
+function remove_line()
+{
+    local file="$1"
+    local line="$2"
+    echo "  Deleting $line from $file"
+    # Filter out the line regardless of whether it has CRLF or LF after it
+    cat "$file" | grep -vxF "$line" | grep -vxF "$line"$'\r' > tmp/compare_compilers/newlist.txt
+    mv tmp/compare_compilers/newlist.txt "$file"
+}
+
+for error_list_file in self_hosted/*s_wrong.txt; do
+    echo "Checking that all files mentioned in $error_list_file exist..."
+    for line in $(grep -v '^#' $error_list_file); do
+        if ! [ -f $line ]; then
+            if [ $fix = yes ]; then
+                remove_line $error_list_file $line
+            else
+                echo "  Error: $error_list_file mentions file \"$line\" which does not exist."
+                echo "  To fix this error, delete the \"$line\" line from $error_list_file (or run again with --fix)."
+                exit 1
+            fi
+        fi
+    done
+done
 
 echo "Compiling the self-hosted compiler..."
 if [[ "$OS" =~ Windows ]]; then
@@ -64,9 +103,7 @@ for action in tokenize parse run; do
         # The file is skipped, so the two compilers should behave differently
         if diff tmp/compare_compilers/compiler_written_in_c.txt tmp/compare_compilers/self_hosted.txt >/dev/null; then
             if [ $fix = yes ]; then
-                echo "  Deleting $file from $error_list_file"
-                grep -vxF $file <(cat $error_list_file | tr -d '\r') > tmp/compare_compilers/newlist.txt
-                mv tmp/compare_compilers/newlist.txt $error_list_file
+                delete_line $error_list_file $file
             else
                 echo "  Error: Compilers behave the same even though the file is listed in $error_list_file."
                 echo "  To fix this error, delete the \"$file\" line from $error_list_file (or run again with --fix)."
@@ -80,8 +117,7 @@ for action in tokenize parse run; do
             echo "  Compilers behave the same as expected"
         else
             if [ $fix = yes ]; then
-                echo "  Adding $file to $error_list_file"
-                echo $file >> $error_list_file
+                append_line $error_list_file $file
             else
                 echo "  Error: Compilers behave differently when given \"$file\"."
                 echo "  Ideally the compilers would behave in the same way for all files, but we aren't there yet."
@@ -89,11 +125,6 @@ for action in tokenize parse run; do
                 exit 1
             fi
         fi
-    fi
-
-    if [ $newline = crlf ] && [ $fix = yes ]; then
-        # Some of the edits (e.g. appending with echo) leave LF line endings
-        unix2dos -q $error_list_file
     fi
 done
 done
