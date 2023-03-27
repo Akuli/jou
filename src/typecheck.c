13b4d7adf4152b9a30bb082a6965b5840101e59e
XiaoBaiYun <71159641+littlewhitecloud@users.noreply.github.com>
2023-03-27 15:24:15 +0800

    I hate this!!!!

--- src/typecheck.c
+++ src/typecheck.c
@@ -795,13 +795,13 @@ static const Type *typecheck_function_or_method_call(FileTypes *ft, const AstCal
     }
 
     char *sigstr = signature_to_string(sig, false);
-    
+
     int n = call->nargs + !!self_type;
     int nargs = sig->nargs;
     if (self_type)
         nargs--;
     if (n < sig->nargs || (n > sig->nargs && !sig->takes_varargs)) {
-		fail_with_error(
+        fail_with_error(
             location,
             "%s %s takes %d argument%s, but it was called with %d argument%s",
             self_type ? "method" : "function",
