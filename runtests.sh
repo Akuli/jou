ad417590a60731904deaf0a2e4adf7ede5af727f
Akuli <akuviljanen17@gmail.com>
2023-12-03 13:01:59 +0200

    ok shellcheck

--- runtests.sh
+++ runtests.sh
@@ -148,7 +148,8 @@ function run_test()
         command="$(printf "$command_template" $joufile)"
     fi
 
-    local diffpath=tmp/tests/diff$(printf "%04d" $counter).txt  # consistent alphabetical order
+    local diffpath
+    diffpath=tmp/tests/diff$(printf "%04d" $counter).txt  # consistent alphabetical order
 
     printf "\n\n\x1b[33m*** Command: %s ***\x1b[0m\n\n" "$command" > $diffpath
 
