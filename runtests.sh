bcaafaeb04da1fdfd4abc71a28f8a298e43d514b
Akuli <akuviljanen17@gmail.com>
2023-12-03 19:03:21 +0200

    memory leak tests

--- runtests.sh
+++ runtests.sh
@@ -148,7 +148,7 @@ function run_test()
     else
         # For non-aoc files we can valgrind the compiled Jou executables.
         # Aoc solutions can be really slow --> valgrind only the compilation.
-        if [ $valgrind = yes ]; then
+        if [ $valgrind = yes ] && [ $correct_exit_code == 0 ]; then
             command="$(printf "$command_template" "--valgrind $joufile")"
         else
             command="$(printf "$command_template" $joufile)"
@@ -166,7 +166,7 @@ function run_test()
     #   * the "test" is actually a GUI program in examples/
     if ( ! [[ "$command_template" =~ -O0 ]] && [[ $joufile =~ ^tests/crash/ ]] ) \
         || ( [[ "$command_template" =~ valgrind ]] && [ $correct_exit_code != 0 ] ) \
-        || [ $joufile = examples/x11_window.jou ]
+        || [ $joufile = examples/x11_window.jou ] || [ $joufile = examples/memory_leak.jou ]
     then
         show_skip $joufile
         mv $diffpath $diffpath.skip
