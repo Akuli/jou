fdf9df2b41f1c399850f369527798af61587ab52
Akuli <akuviljanen17@gmail.com>
2025-01-09 02:22:08 +0200

    Revert "debug"
    This reverts commit bc3e13944f09bd26aa9da09c1f7cf4233d1d0abe.

--- runtests.sh
+++ runtests.sh
@@ -217,6 +217,11 @@ function should_skip()
         return 0
     fi
 
+    # Corner case with Windows paths that I can't figure out.
+    if [ $stage != 3 ] && [[ "$OS" =~ Windows ]] && [[ $joufile =~ double_dotdot_import ]]; then
+        return 0
+    fi
+
     return 1  # false, don't skip
 }
 
@@ -236,9 +241,11 @@ function run_test()
         fi
     else
         if [[ "$OS" =~ Windows ]]; then
+            # I couldn't get PATH to work on windows, so this relies on current working directory
             command="bootstrap\\stage$stage.exe"
         else
-            command="bootstrap/stage$stage"
+            # relies on PATH
+            command="stage$stage"
         fi
     fi
 
@@ -272,8 +279,7 @@ function run_test()
     if $DIFF --text -u $diff_color <(
         generate_expected_output $joufile $correct_exit_code | tr -d '\r'
     ) <(
-        ls -la bootstrap
-        export PATH="$PWD:$PATH"
+        export PATH="$PWD:$PWD/bootstrap:$PATH"
         if [ $valgrind = no ]; then
             ulimit -v 500000 2>/dev/null
         fi
