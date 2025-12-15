a7d96f1bb45b476cf310bd82b7ed3f7061744f2c
Akuli <akuviljanen17@gmail.com>
2025-12-15 20:25:58 +0200

    fix syntax?

--- bootstrap.sh
+++ bootstrap.sh
@@ -132,7 +132,7 @@ function transpile_with_python_and_compile() {
             echo "Patching Jou code to assume aarch64 support exists..."
             grep LLVM_HAS_AARCH64 compiler/target.jou  # fail if there's nothing to replace
             sed -i s/LLVM_HAS_AARCH64/True/g compiler/target.jou
-        else [[ "$OS" =~ Windows ]]; then
+        else
             echo "Patching Jou code to use or not use aarch64 depending on config..."
             sed -i -e s/'if not WINDOWS:'/'if LLVM_HAS_AARCH64:'/g compiler/target.jou
             grep LLVM_HAS_AARCH64 compiler/target.jou  # fail if it replaced nothing
