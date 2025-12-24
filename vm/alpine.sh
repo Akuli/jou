85063a699a6b346c29239ecbe9868b5ccba44259
Akuli <akuviljanen17@gmail.com>
2025-12-24 13:52:12 +0200

    now it seems to work... until it doesn't

--- vm/alpine.sh
+++ vm/alpine.sh
@@ -1,80 +1,150 @@
-#!/bin/sh
+#!/bin/bash
+set -e -o pipefail
 
-set -e
+if [ $# -lt 2 ]; then
+    echo "Usage: $0 <arch> <command inside VM>"
+    exit 1
+fi
 
-sudo apt update
-sudo apt --no-install-recommends install qemu-system-x86 expect
+arch=$1
+shift
+case $arch in
+    x86)
+        qemu=kvm
+        ;;
+    *)
+        echo "$0: Unsupported architecture '$arch' (must be x86)"
+        exit 2
+esac
 
-mkdir -p alpine && cd alpine
+if [ "$GITHUB_ACTIONS" = "true" ]; then
+    if [ $qemu = kvm ]; then
+        qemu='sudo kvm'
+    fi
+    # Don't show any kind of GUI in GitHub Actions
+    qemu="$qemu -nographic -monitor none"
+else
+    # During local development, let the VM stay alive after this script dies
+    qemu="setsid $qemu"
+fi
 
-DOWNLOAD_PATH="https://dl-cdn.alpinelinux.org/latest-stable/releases/x86"
-../download.sh $DOWNLOAD_PATH/alpine-minirootfs-3.23.2-x86.tar.gz 99457a300f13d1971d4627a0ccdf4ae17db18807804e3b5867d4edf8afa16d23
-../download.sh $DOWNLOAD_PATH/alpine-virt-3.23.2-x86.iso 4c6c76a7669c1ec55c6a7e9805b387abdd9bc5df308fd0e4a9c6e6ac028bc1cc
+mkdir -vp "$(dirname "$0")/alpine-$arch"
+cd "$(dirname "$0")/alpine-$arch"
 
-mv -v alpine-minirootfs-*-x86.tar.gz rootfs.tar.gz
-mv -v alpine-virt-*-x86.iso virt.iso
+# rootfs is where Jou will be stored. It is not where we boot from: we need two
+# separate disk images, because the bootable alpine image is too small for
+# installing Jou's dependencies.
+if ! [ -f rootfs-$arch.img ]; then
+    echo "Creating rootfs disk image... (rootfs-$arch.img)"
+    ../download.sh https://dl-cdn.alpinelinux.org/v3.23/releases/$arch/alpine-minirootfs-3.23.2-$arch.tar.gz 99457a300f13d1971d4627a0ccdf4ae17db18807804e3b5867d4edf8afa16d23
+    mkdir rootfs
+    tar xf alpine-minirootfs-3.23.2-$arch.tar.gz -C rootfs
+    rm alpine-minirootfs-3.23.2-$arch.tar.gz
+    truncate -s 4G rootfs-$arch.img
+    /sbin/mkfs.ext4 -q rootfs-$arch.img -d rootfs
+    rm -rf rootfs
+fi
 
-mkdir rootfs
-tar xf rootfs.tar.gz -C rootfs
+# The "virt" image is where we actually boot from.
+if ! [ -f virt-$arch.img ]; then
+    echo "Downloading boot disk... (virt-$arch.img)"
+    ../download.sh https://dl-cdn.alpinelinux.org/v3.23/releases/$arch/alpine-virt-3.23.2-$arch.iso 4c6c76a7669c1ec55c6a7e9805b387abdd9bc5df308fd0e4a9c6e6ac028bc1cc
+    mv -v alpine-virt-3.23.2-$arch.iso virt-$arch.img
+fi
 
-git bundle create rootfs/root/jou.bundle --all
+if [ -f pid.txt ] && kill -0 "$(cat pid.txt)"; then
+    qemu_pid=$(cat pid.txt)
+    echo "qemu is already running (PID $qemu_pid), not restarting."
+else
+    rm -f pid.txt
+    echo "Starting qemu..."
+    # Explanations of qemu options:
+    #   -m: amount of RAM (linux typically uses all available RAM due to disk caching)
+    #   -smp: number of CPUs available inside the VM
+    #   -drive: where to boot from (cdrom), another disk that will appear inside VM
+    #   -nic: enable networking so that port 2222 on host is port 22 (ssh) in VM
+    #   -serial: make serial console available on host's port 4444, used to configure ssh
+    $qemu \
+        -m 1G \
+        -smp $(nproc) \
+        -drive file=virt-$arch.img,format=raw,media=cdrom \
+        -drive file=rootfs-$arch.img,format=raw \
+        -nic user,hostfwd=tcp:127.0.0.1:2222-:22 \
+        -serial tcp:localhost:4444,server=on,wait=off \
+        &
+    qemu_pid=$!
+    echo $qemu_pid > pid.txt
+    disown
+    sleep 1
+    kill -0 $qemu_pid  # stop if qemu died instantly
+fi
 
-truncate -s 4G rootfs.img
-/sbin/mkfs.ext4 rootfs.img -d rootfs
-rm -rf rootfs
+ssh="ssh root@localhost -o StrictHostKeyChecking=no -o UserKnownHostsFile=my_known_hosts -i key -p 2222"
 
-expect <<END
-set timeout 300
+echo "Checking if ssh works..."
+if ! [ -f key ] || ! timeout 5 $ssh echo hello; then
+    # ssh doesn't work either because we didn't set it up yet or we need to
+    # wait for the VM to start.
+    #
+    # We consider the VM started when it shows login prompt on serial port.
+    # At that point it has also started ssh.
+    echo "Waiting for VM to boot..."
+    until echo | ../wait_for_string.sh 'localhost login:' nc localhost 4444; do sleep 1; done
+    echo "Checking again if ssh works..."
+    if ! [ -f key ] || ! timeout 5 $ssh echo hello; then
+        echo "ssh doesn't work. Let's set it up using serial port."
+        (yes || true) | ssh-keygen -t ed25519 -f key -N ''
+        # Log in as root
+        echo root | ../wait_for_string.sh 'localhost:~#' nc localhost 4444
+        # Go to our rootfs disk with chroot and set up ssh
+        echo "
+mount -t ext4 /dev/sda /mnt
+cd /mnt
+mount -t proc /proc proc/
+mount -t sysfs /sys sys/
+mount --rbind /dev dev/
+mount --rbind /run run/
+chroot .
+echo 'iface eth0 inet dhcp' > /etc/network/interfaces
+ifup eth0
+apk update
+apk add openssh-server
+ssh-keygen -A
+chown root:root /var/empty  # don't know why sshd wants this
+/usr/sbin/sshd
+mkdir ~/.ssh
+echo '$(cat key.pub)' > ~/.ssh/authorized_keys
+chown root:root ~
+chmod 700 ~/.ssh
+chmod 600 ~/.ssh/authorized_keys
+exit
+echo ALL'DONE'NOW
+exit
+" | ../wait_for_string.sh ALLDONENOW nc localhost 4444
+        echo "Now ssh setup is done, let's check one last time..."
+        $ssh echo hello  # Check that it works
+    fi
+fi
 
-spawn sudo kvm -m 2G -smp 2 -drive file=virt.iso,format=raw,media=cdrom -drive file=rootfs.img,format=raw -nographic
+echo "Checking if repo needs to be copied over..."
+if [ "$($ssh 'cd jou && git rev-parse HEAD' || true)" != "$(git rev-parse HEAD)" ]; then
+    echo "Installing packages (if not already installed)..."
+    $ssh 'which git || apk add bash clang llvm-dev make git grep libx11-dev'
 
-expect {
-    "localhost login: " {
-        send "root\r"
-    }
-}
-expect {
-    "localhost:~# " {
-        send "mount -t ext4 /dev/sda /mnt && cd /mnt && "
-        send "mount -t proc /proc proc/ && "
-        send "mount -t sysfs /sys sys/ && "
-        send "mount --rbind /dev dev/ && "
-        send "mount --rbind /run run/ && "
-        send "chroot . \r"
-    }
-}
-expect {
-    "/ # " {
-        send "cd \r"
-    }
-}
-expect {
-    "~ # " {
-        send "echo 'FAIL!!' >status && echo 'iface eth0 inet dhcp' >/etc/network/interfaces && ifup eth0 && "
-        send "apk update && apk upgrade && TERM=dumb apk add bash clang llvm-dev make git grep patch libx11-dev && "
-        send "git clone ./jou.bundle jou && cd jou && "
-        send "./runtests.sh --verbose && mv jou jou_bootstrap && ./runtests.sh should_succeed && ./doctest.sh && "
-        send "echo SUCCESS >~/status \r"
-    }
-}
-expect {
-    "~ # " {
-        exit 1
-    }
-    "~/jou # " {
-        send "cat ~/status \r"
-    }
-    timeout {
-        exit 67
-    }
-}
-expect {
-    "SUCCESS" {
-        exit 0
-    }
-    "FAIL!!" {
-        exit 1
-    }
-}
-wait
-END
+    echo "Copying repository to VM..."
+    git bundle create jou.bundle --all
+    cat jou.bundle | $ssh 'cat > jou.bundle'  # easier and faster than using scp here
+    rm jou.bundle
+
+    echo "Checking out repository inside VM..."
+    $ssh "
+    set -e
+    [ -d jou ] || git init jou
+    cd jou
+    git fetch ../jou.bundle
+    git checkout -f $(git rev-parse HEAD)  # The rev-parse runs on host, not inside VM
+    "
+fi
+
+echo "Running command in VM's jou folder: $@"
+$ssh cd jou '&&' "$@"
--- vm/netbsd.sh
+++ vm/netbsd.sh
@@ -20,13 +20,9 @@ esac
 
 if [ "$GITHUB_ACTIONS" = "true" ]; then
     if [ $qemu = kvm ]; then
-        # I think sudo shouldn't be needed when developing locally, because you
-        # can add yourself to the kvm group.
-        #
-        # Please make a GitHub issue if I am wrong.
         qemu='sudo kvm'
     fi
-    # In GitHub Actions, don't show any kind of GUI
+    # Don't show any kind of GUI in GitHub Actions
     qemu="$qemu -nographic -monitor none"
 else
     # During local development, let the VM stay alive after this script dies
@@ -52,14 +48,14 @@ if ! [ -f NetBSD-10.1-$arch-live.img ]; then
     (dd if=NetBSD-10.1-amd64-live.img bs=1 skip=$offset count=1000 status=none || true) | head -1  # show the line of text
     printf "on " | dd of=NetBSD-10.1-amd64-live.img bs=1 seek=$((offset+44)) conv=notrunc status=none
     (dd if=NetBSD-10.1-amd64-live.img bs=1 skip=$offset count=1000 status=none || true) | head -1  # show the line of text
-fi
 
-# Make disk image large enough for LLVM and other tools.
-#
-# When NetBSD boots, it detects that the disk has been resized, adjusts its
-# partitions to use the whole disk, and automatically reboots.
-echo "Resizing disk..."
-truncate -s 4G NetBSD-10.1-$arch-live.img
+    # Make disk image large enough for LLVM and other tools.
+    #
+    # When NetBSD boots, it detects that the disk has been resized, adjusts its
+    # partitions to use the whole disk, and automatically reboots.
+    echo "Resizing disk..."
+    truncate -s 4G NetBSD-10.1-$arch-live.img
+fi
 
 if [ -f pid.txt ] && kill -0 "$(cat pid.txt)"; then
     qemu_pid=$(cat pid.txt)
