88388b435a255244b606cff8ba84f9ff2b215bb0
Akuli <akuviljanen17@gmail.com>
2026-02-28 20:10:56 +0200

    a bunch more passing

--- toml/run.sh
+++ toml/run.sh
@@ -7,10 +7,28 @@ set -e
 jou -o toml_parser toml_parser.jou
 
 # Reasons to skip:
-#   valid/string/unicode-escape: uses \u0000 and \U00000000 which are not supported
+#   valid/spec-example-1-compact: contains a date
+#   valid/spec-example-1: contains a date
 #   valid/string/quoted-unicode: uses \u0000 which is not supported
-./toml-test-* test -decoder ./toml_parser -run "valid/string/*" \
+#   valid/string/unicode-escape: uses \u0000 and \U00000000 which are not supported
+./toml-test-* test -decoder ./toml_parser -run "valid/a*"
+./toml-test-* test -decoder ./toml_parser -run "valid/b*"
+./toml-test-* test -decoder ./toml_parser -run "valid/c*"
+./toml-test-* test -decoder ./toml_parser -run "valid/d*"
+./toml-test-* test -decoder ./toml_parser -run "valid/e*" \
+    -skip valid/example \
+    -skip-must-err
+./toml-test-* test -decoder ./toml_parser -run "valid/f*"
+./toml-test-* test -decoder ./toml_parser -run "valid/g*"
+./toml-test-* test -decoder ./toml_parser -run "valid/h*"
+./toml-test-* test -decoder ./toml_parser -run "valid/i*"
+./toml-test-* test -decoder ./toml_parser -run "valid/j*"
+./toml-test-* test -decoder ./toml_parser -run "valid/k*"
+./toml-test-* test -decoder ./toml_parser -run "valid/l*"
+./toml-test-* test -decoder ./toml_parser -run "valid/m*"
+./toml-test-* test -decoder ./toml_parser -run "valid/s*" \
+    -skip valid/spec-example-1-compact \
+    -skip valid/spec-example-1 \
+    -skip valid/string/quoted-unicode \
     -skip valid/string/unicode-escape \
-    -skip valid/string/quoted-unicode
-
-./toml-test-* test -decoder ./toml_parser -run "valid/multibyte"
+    -skip-must-err
