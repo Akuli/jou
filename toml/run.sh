29eaa5bc9d5dbe9da1c09d3bbbdbe75fd658584a
Akuli <akuviljanen17@gmail.com>
2026-02-28 22:28:23 +0200

    92 valid and 44 invalid failing

--- toml/run.sh
+++ toml/run.sh
@@ -6,15 +6,8 @@ set -e
 
 jou -o toml_parser toml_parser.jou
 
-# Reasons to skip:
-#   valid/spec-example-1-compact: contains a date
-#   valid/spec-example-1: contains a date
-#   valid/string/quoted-unicode: uses \u0000 which is not supported
-#   valid/string/unicode-escape: uses \u0000 and \U00000000 which are not supported
-./toml-test-* test -decoder ./toml_parser -run "valid/*" \
-    -skip valid/example \
-    -skip valid/spec-example-1-compact \
-    -skip valid/spec-example-1 \
-    -skip valid/string/quoted-unicode \
-    -skip valid/string/unicode-escape \
-    -skip-must-err
+tests_to_skip=(
+    valid/spec-example-1            # contains a date
+    valid/spec-example-1-compact    # contains a date
+)
+./toml-test-* test -decoder ./toml_parser $(printf " -skip %s" ${tests_to_skip[@]})
