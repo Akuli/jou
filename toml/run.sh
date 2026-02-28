7d94c9e828678f95c0c20f4124ecbe53cb2f51cd
Akuli <akuviljanen17@gmail.com>
2026-02-28 20:13:24 +0200

    run more of them

--- toml/run.sh
+++ toml/run.sh
@@ -11,22 +11,8 @@ jou -o toml_parser toml_parser.jou
 #   valid/spec-example-1: contains a date
 #   valid/string/quoted-unicode: uses \u0000 which is not supported
 #   valid/string/unicode-escape: uses \u0000 and \U00000000 which are not supported
-./toml-test-* test -decoder ./toml_parser -run "valid/a*"
-./toml-test-* test -decoder ./toml_parser -run "valid/b*"
-./toml-test-* test -decoder ./toml_parser -run "valid/c*"
-./toml-test-* test -decoder ./toml_parser -run "valid/d*"
-./toml-test-* test -decoder ./toml_parser -run "valid/e*" \
+./toml-test-* test -decoder ./toml_parser -run "valid/*" \
     -skip valid/example \
-    -skip-must-err
-./toml-test-* test -decoder ./toml_parser -run "valid/f*"
-./toml-test-* test -decoder ./toml_parser -run "valid/g*"
-./toml-test-* test -decoder ./toml_parser -run "valid/h*"
-./toml-test-* test -decoder ./toml_parser -run "valid/i*"
-./toml-test-* test -decoder ./toml_parser -run "valid/j*"
-./toml-test-* test -decoder ./toml_parser -run "valid/k*"
-./toml-test-* test -decoder ./toml_parser -run "valid/l*"
-./toml-test-* test -decoder ./toml_parser -run "valid/m*"
-./toml-test-* test -decoder ./toml_parser -run "valid/s*" \
     -skip valid/spec-example-1-compact \
     -skip valid/spec-example-1 \
     -skip valid/string/quoted-unicode \
