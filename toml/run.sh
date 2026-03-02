4f9338f9ab63dcad7b158d9e9788d8536f97758d
Akuli <akuviljanen17@gmail.com>
2026-03-02 20:53:22 +0200

    Set TOML version properly

--- toml/run.sh
+++ toml/run.sh
@@ -4,11 +4,14 @@ set -e
 jou -o toml_parser toml_parser.jou
 
 # Make sure all error messages are mentioned in expected_errors.toml
-diff -u --color=always <(./toml-test-* list | grep ^invalid) <(grep '^"' expected_errors.toml | cut -d'"' -f2)
+# The language version (-toml 1.1) is specified again below in this script.
+diff -u --color=always <(./toml-test-* list -toml 1.1 | grep ^invalid) <(grep '^"' expected_errors.toml | cut -d'"' -f2)
 
 tests_to_skip=(
-    # These tests contain dates or times, so they are not supported
+    # TODO: go through these and figure out what happens in each one
     valid/array/array
+    valid/array/array-subtables
+    valid/array/open-parent-table
     valid/comment/everywhere
     valid/datetime/datetime
     valid/datetime/edge
@@ -17,28 +20,26 @@ tests_to_skip=(
     valid/datetime/local-date
     valid/datetime/local-time
     valid/datetime/milliseconds
+    valid/datetime/no-seconds
     valid/datetime/timezone
     valid/example
-    valid/spec-1.0.0/local-date-0
-    valid/spec-1.0.0/local-date-time-0
-    valid/spec-1.0.0/local-time-0
-    valid/spec-1.0.0/offset-date-time-0
-    valid/spec-1.0.0/offset-date-time-1
-    valid/spec-1.0.0/table-7
-    valid/spec-example-1
-    valid/spec-example-1-compact
-
-    # Contains zero byte in a key, which is not supported
-    valid/key/quoted-unicode
-
-    # TODO: Figure out why these fail, fix as appropriate
-    valid/array/array-subtables
-    valid/array/open-parent-table
     valid/inline-table/key-dotted-04
     valid/key/dotted-04
     valid/key/like-date
-    valid/spec-1.0.0/array-of-tables-0
-    valid/spec-1.0.0/array-of-tables-1
+    valid/key/quoted-unicode
+    valid/spec-1.1.0/common-27
+    valid/spec-1.1.0/common-28
+    valid/spec-1.1.0/common-29
+    valid/spec-1.1.0/common-30
+    valid/spec-1.1.0/common-31
+    valid/spec-1.1.0/common-32
+    valid/spec-1.1.0/common-33
+    valid/spec-1.1.0/common-34
+    valid/spec-1.1.0/common-44
+    valid/spec-1.1.0/common-51
+    valid/spec-1.1.0/common-52
+    valid/spec-example-1
+    valid/spec-example-1-compact
     valid/table/array-empty
     valid/table/array-empty-name
     valid/table/array-implicit
@@ -48,39 +49,34 @@ tests_to_skip=(
     valid/table/array-one
     valid/table/array-table-array
     valid/table/array-within-dotted
-
     invalid/array/extend-defined-aot
     invalid/array/extending-table
     invalid/array/tables-01
     invalid/array/tables-02
+    invalid/control/multi-cr
+    invalid/control/rawmulti-cr
     invalid/datetime/y10k
     invalid/inline-table/duplicate-key-03
     invalid/inline-table/duplicate-key-04
-    invalid/inline-table/linebreak-01
-    invalid/inline-table/linebreak-02
-    invalid/inline-table/linebreak-03
-    invalid/inline-table/linebreak-04
     invalid/inline-table/overwrite-02
     invalid/inline-table/overwrite-04
     invalid/inline-table/overwrite-05
     invalid/inline-table/overwrite-06
     invalid/inline-table/overwrite-07
     invalid/inline-table/overwrite-08
-    invalid/inline-table/trailing-comma
     invalid/integer/double-us
     invalid/key/after-array
     invalid/key/dotted-redefine-table-01
     invalid/key/dotted-redefine-table-02
     invalid/key/no-eol-04
-    invalid/spec-1.0.0/inline-table-2-0
-    invalid/spec-1.0.0/inline-table-3-0
-    invalid/spec-1.0.0/key-value-pair-1
-    invalid/spec-1.0.0/keys-2
-    invalid/spec-1.0.0/string-4-0
-    invalid/spec-1.0.0/string-7-0
-    invalid/spec-1.0.0/table-9-0
-    invalid/spec-1.0.0/table-9-1
-    invalid/string/basic-byte-escapes
+    invalid/spec-1.1.0/common-16-0
+    invalid/spec-1.1.0/common-19-0
+    invalid/spec-1.1.0/common-2
+    invalid/spec-1.1.0/common-46-0
+    invalid/spec-1.1.0/common-46-1
+    invalid/spec-1.1.0/common-49-0
+    invalid/spec-1.1.0/common-5
+    invalid/spec-1.1.0/common-50-0
     invalid/table/append-with-dotted-keys-01
     invalid/table/append-with-dotted-keys-02
     invalid/table/append-with-dotted-keys-03
@@ -123,4 +119,4 @@ tests_to_skip=(
     invalid/table/super-twice
 )
 
-./toml-test-* test -decoder ./toml_parser -skip-must-err -errors expected_errors.toml $(printf " -skip %s" ${tests_to_skip[@]}) "$@"
+./toml-test-* test -decoder ./toml_parser -toml 1.1 -skip-must-err -errors expected_errors.toml $(printf " -skip %s" ${tests_to_skip[@]}) "$@"
