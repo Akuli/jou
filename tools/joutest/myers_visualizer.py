68fba241737faaf54608efac81e92abe72a812bd
Akuli <akuviljanen17@gmail.com>
2026-03-18 23:13:25 +0200

    Add Python script that helps me understand how myers diff algorithm works

---
+++ tools/joutest/myers_visualizer.py
@@ -0,0 +1,121 @@
+# Visualizes the algorithm in "FIGURE 2" of the Myers 1986 paper. The paper is
+# named "An O(ND) Difference Algorithm and Its Variations" and can easily be
+# found online.
+
+import tkinter
+
+# We want to compute the diff that represents changing a to b.
+# This is the example from the Myers paper.
+a = "abcabba"
+b = "cbabac"
+
+scale = 50
+
+canvas = tkinter.Canvas(width=(len(a) + 4)*scale, height=(len(b) + 4)*scale, bg="black")
+canvas.pack(fill="both", expand=True)
+
+def point_to_canvas(x, y):
+    return ((x + 1)*scale, (y + 1)*scale)
+
+# Draw background grid
+for x in range(len(a) + 1):
+    canvas.create_line(point_to_canvas(x, 0), point_to_canvas(x, len(b)), fill="#444")
+for y in range(len(b) + 1):
+    canvas.create_line(point_to_canvas(0, y), point_to_canvas(len(a), y), fill="#444")
+
+# Mark rows and columns of grid with letters
+for i, letter in enumerate(a):
+    canvas.create_text(point_to_canvas(i + 0.5, -0.2), text=letter, fill="#444")
+for i, letter in enumerate(b):
+    canvas.create_text(point_to_canvas(-0.2, i + 0.5), text=letter, fill="#444")
+
+def draw_arrow(x1, y1, x2, y2, highlight=False) -> None:
+    # Don't go all the way to (x2,y2) so there's room for text
+    dx = x2 - x1
+    dy = y2 - y1
+    x1 += 0.2*dx
+    y1 += 0.2*dy
+    x2 -= 0.2*dx
+    y2 -= 0.2*dy
+
+    return canvas.create_line(
+        *point_to_canvas(x1, y1), *point_to_canvas(x2, y2),
+        width=2, arrow="last", fill="white",
+    )
+
+def draw_text(x, y, text) -> None:
+    canvas.create_text(point_to_canvas(x, y), text=text, fill="white")
+
+# This is the code from the original Myers paper (FIGURE 2) with the
+# following modifications:
+#   - Less clever and more explicit way to start the algorithm
+#   - Arrow drawing added
+#   - Backtracking to find out the diff by keeping track of arrows drawn
+MAX = len(a) + len(b)
+V = [None] * (2*MAX + 1)  # Python's negative indexing will be used
+arrows = []
+
+# D is the number of non-diagonal arrows
+for D in range(MAX + 1):
+    for k in range(-D, D + 1, 2):
+        if D == 0:
+            # No arrows yet
+            x = y = 0
+        elif k == -D or (k != D and V[k-1] < V[k+1]):
+            # Arrow down (x doesn't change, y increases, k=x-y decreases)
+            # This is an addition because it consumes b without consuming a.
+            x = V[k+1]
+            y = x-k
+            draw_text(x, y, D)
+            arrows.append((x, y, '+', draw_arrow(x, y-1, x, y)))
+        else:
+            # Arrow right (x increases, y doesn't change, k=x-y increases)
+            # This is a removal because it consumes a without consuming b.
+            x = V[k-1] + 1
+            y = x-k
+            draw_text(x, y, D)
+            arrows.append((x, y, '-', draw_arrow(x-1, y, x, y)))
+
+        while x < len(a) and y < len(b) and a[x] == b[y]:
+            # Diagonal arrow (x increases, y increases, k=x-y doesn't change)
+            # These are unchanged lines because they consume both strings.
+            (x, y) = (x+1, y+1)
+            draw_text(x, y, D)
+            arrows.append((x, y, ' ', draw_arrow(x-1, y-1, x, y)))
+
+        V[k] = x
+        if x == len(a) and y == len(b):
+            # Now we need to backtrack through the algorithm to figure out
+            # the optimal path from top left corner to bottom right corner.
+            #
+            # I asked an LLM to prove mathematically that the end point of
+            # an arrow uniquely identifies an arrow. It gave a convincing
+            # looking proof, so let's assume that's true. So we can just
+            # find the arrow that ends at (x,y), follow it backwards, and
+            # repeat until we arrive at (0,0).
+            #
+            # This wouldn't benefit much from using a hashmap, because the
+            # arrows are added in order, so it's fast enough to find them
+            # by going through the list backwards.
+            print("Diff in reverse order:")
+            while x != 0 or y != 0:
+                while arrows[-1][:2] != (x, y):
+                    arrows.pop()
+                letter, arrow_id = arrows.pop()[2:]
+                canvas.itemconfig(arrow_id, fill="cyan")
+                match letter:
+                    case '+':
+                        y -= 1
+                        print(f"+{b[y]}")
+                    case '-':
+                        x -= 1
+                        print(f"-{a[x]}")
+                    case ' ':
+                        x -= 1
+                        y -= 1
+                        assert a[x] == b[y]
+                        print(f" {a[x]}")
+                    case _:
+                        raise ValueError("oh no")
+            tkinter.mainloop()
+            exit()
