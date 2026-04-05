822e8b0f2daf85c6d85d4ef5a3f354df07131243
Akuli <akuviljanen17@gmail.com>
2026-04-05 12:18:12 +0300

    better check script

--- check.sh
+++ check.sh
@@ -4,4 +4,8 @@ set -ex
 wine ~/wjou/jou.exe -o joutest.exe tools/joutest/main.jou
 
 cd tests/tests_of_joutest/should_succeed/basic_ok
-wine ../../../../joutest.exe
+echo '
+import "stdlib/io.jou"
+def main() -> int:
+    puts("wrong")
+    return 0' | wine ../../../../joutest.exe
