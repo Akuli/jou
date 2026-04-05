ffc5e9599b95dac183a5416931121bff7572c864
Akuli <akuviljanen17@gmail.com>
2026-04-05 19:06:24 +0300

    more temp debug, gets stuck for some reason

--- check.sh
+++ check.sh
@@ -3,9 +3,5 @@ set -ex
 
 wine ~/wjou/jou.exe -o joutest.exe tools/joutest/main.jou
 
-cd tests/tests_of_joutest/should_succeed/basic_ok
-echo '
-import "stdlib/io.jou"
-def main() -> int:
-    puts("wrong")
-    return 0' | wine ../../../../joutest.exe
+cd tests/tests_of_joutest/should_succeed/markdown
+wine ../../../../joutest.exe -v
